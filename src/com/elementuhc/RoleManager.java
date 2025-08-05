e8229bb9d7cb8dd67680c45b058e5655330522c8
Kategaia_ <ethangoudet@gmail.com>
2025-08-05 23:42:43 +0200

    fonction attribution aléatoire des rôles

--- src/com/elementuhc/RoleManager.java
+++ src/com/elementuhc/RoleManager.java
@@ -2,18 +2,28 @@ package com.elementuhc;
 
 import org.bukkit.entity.Player;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class RoleManager {
-    private Map<Player, Role> playerRoles = new HashMap<>();
+    private final Map<Player, Role> playerRoles = new HashMap<>();
+    private final Queue<RoleType> shuffledRoles = new LinkedList<>();
 
-    public void assignRole(Player player, RoleType type) {
+    public void prepareRoles() {
+        List<RoleType> roles = new ArrayList<>(List.of(RoleType.values()));
+        Collections.shuffle(roles);
+        shuffledRoles.clear();
+        shuffledRoles.addAll(roles);
+    }
+
+    public void assignNextRole(Player player) {
+        if(shuffledRoles.isEmpty()) {
+            return;
+        }
+
+        RoleType type = shuffledRoles.poll();
         Role role = new Role(type);
         playerRoles.put(player, role);
-        player.sendMessage("§aVotre rôle : §e" + role.getName());
-        player.sendMessage("§7" + role.getDescription());
+        player.sendMessage("§a Votre rôle : §e" + role.getName());
     }
 
     public Role getRole(Player player) {
